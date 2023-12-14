d185a902fa376874a93f4b71e7676a2aa0b95a79
JuliaMathias <juliamss@gmail.com>
2023-12-14 19:25:45 -0300

    solve Cars Assemble java exercise

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,30 @@
 public class CarsAssemble {
 
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRateperHour() method");
+        int carsPerHour = 221;
+        int production = carsPerHour * speed;
+
+        if (speed >= 1 && speed <= 4) {
+            double productionRatePerHour = production;
+            return productionRatePerHour;
+        } else if (speed >= 5 && speed <= 8) {
+            double productionRatePerHour = production * 0.9;
+            return productionRatePerHour;
+        } else if (speed == 9) {
+            double productionRatePerHour = production * 0.8;
+            return productionRatePerHour;
+        } else if (speed == 10) {
+            double productionRatePerHour = production * 0.77;
+            return productionRatePerHour;
+        } else {
+            return 0;
+        }
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+        double workingCarsPerHour = productionRatePerHour(speed);
+        double workingCarsPerMinute = workingCarsPerHour / 60;
+        int workingItemsPerMinute = (int) Math.floor(workingCarsPerMinute);
+        return workingItemsPerMinute;
     }
 }
